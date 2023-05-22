bad904e923d7b07a9eebbe9765520cd70468fe31
Faizan59khan <khan_faizan59@yahoo.com>
2023-05-22 17:59:34 +0500

    Added Data Fetch Methods

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import Link from "next/link";
 
-export default function Home() {
-  return <main className="p-5">home</main>;
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+export default async function Home() {
+ 
+
+  return (
+    <main className="p-5">
+      <h1>Home</h1>
+    </main>
+  );
 }
