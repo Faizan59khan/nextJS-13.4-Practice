e13d834c12ba52cce9af38d80bc903112faa73d6
Faizan59khan <khan_faizan59@yahoo.com>
2023-05-24 18:08:30 +0500

    Establish Mongodb Connection

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,6 @@
-interface User{
+import { MongoClient } from "mongodb";
+
+interface User {
   id: number;
   name: string;
 }
@@ -6,11 +8,31 @@ interface User{
 export async function GET() {
   // Handle GET request for /api/users
   // Retrieve users from the database or any data source
-  const Users: User[]=[
-    {id: 1, name: 'Faizan'},
-    {id: 2, name: 'Salman'},
-    {id: 3, name: 'Kashif'},
-  ]
-  return new Response(JSON.stringify(Users));
-}
 
+  try {
+    // Check if the DATA_API_KEY is defined
+    if (!process.env.DATA_API_KEY) {
+      throw new Error("DATA_API_KEY environment variable is not set.");
+    }
+
+    // Connect to MongoDB Atlas
+    const uri = process.env.DATA_API_KEY; // Use your API key here
+    const client = new MongoClient(uri);
+    await client.connect();
+
+    // Retrieve users from MongoDB collection
+    const db = client.db("Next13");
+    const collection = db.collection("Users");
+    const users = await collection.find().toArray();
+
+    // Close the MongoDB connection
+    await client.close();
+
+    return new Response(JSON.stringify(users));
+  } catch (err) {
+    const error = err as Error;
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
